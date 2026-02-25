0440145067a62fb6b397f6aa73e511b90ea04507
Turaka Prudvi <68219624+prudviraj12345@users.noreply.github.com>
2026-02-25 11:20:51 +0530

    Add files via upload

---
+++ frontend/src/src/App.tsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="*" element={<Index />} />
+    </Routes>
+  );
+}
+
+export default App;
